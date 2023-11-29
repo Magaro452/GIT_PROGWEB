baf118978014dc0a16ca8814264906f969056467
unknown <almandozmara5 git config --global user.email almandozmaria5gmail.com>
2023-11-29 20:53:02 -0300

    creación de nuevos archivos, eliminación y modificación de otros

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function verMas(elemento, event) {
+   
+    if (elemento.style.height !="fit-content") {
+        elemento.style.height ="fit-content";
+    } else{
+        elemento.style.height = "50px";   
+    }
+}
